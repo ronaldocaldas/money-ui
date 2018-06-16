dd867bc2017e34dc851c5efab4d1de16331e7e69
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2018-06-16 20:44:12 -0300

    Implementação do cadastro de pessoas

--- src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
+++ src/app/pessoas/pessoa-cadastro/pessoa-cadastro.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ToastyService } from 'ng2-toasty';
+
+import { PessoaService } from './../pessoa.service';
+import { ErrorHandlerService } from '../../core/error-handler.service';
+import { Pessoa } from '../../core/model';
 
 @Component({
   selector: 'app-pessoa-cadastro',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PessoaCadastroComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private pessoaService: PessoaService,
+    private toastyService: ToastyService,
+    private errorHandler: ErrorHandlerService) { }
+
+  pessoa = new Pessoa();
 
   ngOnInit() {
   }
 
+  salvar(form: FormControl) {
+    this.pessoaService.adicionar(this.pessoa)
+    .then(() => {
+      this.toastyService.success('Pessoa adicionada com sucesso!');
+      form.reset();
+      this.pessoa = new Pessoa();
+    })
+    .catch(erro  => this.errorHandler.handler(erro));
+  }
 }
--- src/app/pessoas/pessoa.service.ts
+++ src/app/pessoas/pessoa.service.ts
@@ -1,3 +1,4 @@
+import { Pessoa } from './../core/model';
 import { Http, Headers, URLSearchParams } from '@angular/http';
 import { Injectable } from '@angular/core';
 
@@ -75,5 +76,15 @@ export class PessoaService {
     return this.http.get(`${this.pessoasUrl}`, { headers })
       .toPromise()
       .then(response => response.json().content);
+  }
+
+  adicionar(pessoa: Pessoa): Promise<Pessoa> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YWRtaW5AbW9uZXkuY29tOmFkbWlu');
+    headers.append('Content-Type', 'application/Json');
+
+    return this.http.post(this.pessoasUrl, JSON.stringify(pessoa), {headers})
+      .toPromise()
+      .then(response => response.json());
   }
 }
